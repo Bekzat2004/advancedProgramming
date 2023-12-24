c0ef69631c8096cfb5f1b82adca3b4c87b1d80ea
Bekzat2004 <121132251+Bekzat2004@users.noreply.github.com>
2023-12-24 23:55:19 +0600

    Add files via upload

---
+++ firstProject/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type JsonRequest struct {
+	Message string `json:"message"`
+}
+
+type JsonResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+func main() {
+	http.HandleFunc("/", handlePostRequest)
+	port := 8080
+	fmt.Printf("Server listening on :%d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
+
+func handlePostRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var requestData JsonRequest
+	err := decoder.Decode(&requestData)
+	if err != nil {
+		http.Error(w, "Invalid JSON message", http.StatusBadRequest)
+		return
+	}
+
+	if requestData.Message == "" {
+		http.Error(w, "Invalid JSON message", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("Received message: %s\n", requestData.Message)
+
+	response := JsonResponse{
+		Status:  "success",
+		Message: "Data successfully received",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
